7b16709a0ec7b4c61bc34bfb921d7d0a102d0fb0
motaigr <igor10.mota@hotmail.com>
2026-07-09 16:06:04 -0300

    tarefas semana 02

---
+++ semana02/scripts/bom.js
@@ -0,0 +1,23 @@
+const input = document.querySelector('#favchap');
+const botao = document.querySelector('button');
+const lista = document.querySelector('#list');
+
+botao.addEventListener('click', function() {
+  if (input.value.trim() !== '') {
+    const li = document.createElement('li');
+    const botaoExcluir = document.createElement('button');
+    li.textContent = input.value;
+    botaoExcluir.textContent = '❌';
+    botaoExcluir.setAttribute('aria-label', `Excluir ${input.value}`);
+    li.append(botaoExcluir);
+    lista.append(li);
+
+    botaoExcluir.addEventListener('click', function() {
+      lista.removeChild(li);
+      input.focus();
+    });
+
+    input.value = '';
+  }
+  input.focus(); 
+});
